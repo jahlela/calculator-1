1ed8e7a9089ffe5a21fdebaa89e196b0c8c2e371
Hackbright Student <no-reply@hackbrightacademy.com>
2016-09-28 12:31:22 -0700

    Added docstrings

---
+++ arithmetic.py
@@ -0,0 +1,33 @@
+def add(num1, num2):
+    """ Returns the sum of the inputs"""
+    return 10
+
+def subtract(num1, num2):
+	"""Returns the result of subtracting the second number from the first"""
+    pass
+
+def multiply(num1, num2):
+	"""Returns the product of the inputs"""
+    pass
+
+def divide(num1, num2):	
+	"""Divides the first input by the second, returning a floating point number"""
+    pass
+
+def square(num1):
+	"""Returns the square of the input"""
+    pass
+
+def cube(num1):
+	"""Returns the cube of the input"""
+    pass
+
+def power(num1, num2):
+	"""Returns the result of raising the first input to the power of the second input"""
+    pass
+
+def mod(num1, num2):
+	"""Returns the remainder when the first input is divided by the second input"""
+    pass
+
+print subtract(6,3)
